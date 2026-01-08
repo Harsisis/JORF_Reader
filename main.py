81f4d6b2f5af39599373a64f3c71a007205b3edc
Harsisis <56965488+Harsisis@users.noreply.github.com>
2026-01-08 19:49:30 +0100

    JORF_READER: update main.py file

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#JORF_MANAGER.download_tar_gz_files()
-#JORF_MANAGER.read_tar_gz_files()
+JORF_MANAGER.download_tar_gz_files()
+JORF_MANAGER.read_tar_gz_files()
 data = JORF_MANAGER.read_xml_folder()
 
 ExcelManager.save_to_excel(data)
